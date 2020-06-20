ddcfff0976273f5a0d4099ec9a318a9550e32df1
Spanaic <spanaic@gmail.com>
2020-06-20 22:40:18 +0900

    ユーザーの入力値エラーをテストするコードを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -79,14 +79,30 @@ RSpec.describe ProjectsController, type: :controller do
         @user = FactoryBot.create(:user)
       end
 
-      # プロジェクトを追加できること
-      it 'adds a project' do
-        project_params = FactoryBot.attributes_for(:project)
-        sign_in @user
-        expect {
-          post :create, params: { project: project_params }
-        }.to change(@user.projects, :count).by(1)
+      # 有効な属性値の場合
+      context "with valid attributes" do
+        # プロジェクトを追加できること
+        it 'adds a project' do
+          project_params = FactoryBot.attributes_for(:project)
+          sign_in @user
+          expect {
+            post :create, params: { project: project_params }
+          }.to change(@user.projects, :count).by(1)
+        end
       end
+
+      # 無効な属性値の場合
+      context "with invalid attributes" do
+        # プロジェクトを追加出来ないこと
+        it "does not add a project" do
+          sign_in @user
+          project_params = FactoryBot.attributes_for(:project, :invalid)
+          expect {
+            post :create, params: { project: project_params }
+          }.to_not change(Project, :count)
+        end
+      end
+
     end
 
     # ゲストとして
