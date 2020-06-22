9a66bedd64ea622860548fcb776c21fb99525a2d
Spanaic <spanaic@gmail.com>
2020-06-23 08:46:44 +0900

    taskとnoteのbeforeをletに書き換え

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -8,19 +8,9 @@ RSpec.describe Note, type: :model do
     puts "This note's user is #{note.user.inspect}"
   end
 
-  before do
-    # このファイルの全テストで使用するテストデータをセットアップする
-    @user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "tester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    @project = @user.projects.create(
-      name: "Test Project",
-    )
-  end
+  # beforeの中をletにリファクタリング
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project) }
 
   # バリデーション用のスペックが並ぶ
 
@@ -28,8 +18,8 @@ RSpec.describe Note, type: :model do
   it "is valid with a user, project, and message" do
     note = Note.new(
       message: "This is a sample note.",
-      user: @user,
-      project: @project,
+      user: user,
+      project: project,
     )
 
     expect(note).to be_valid
@@ -45,30 +35,34 @@ RSpec.describe Note, type: :model do
   # 文字列に一致するメッセージを検索する
   describe "search message for a term" do
 
-    before do
-      # 検索機能の全テストに関連する追加のテストデータをセットアップする
-      @note1 = @project.notes.create(
-        message: "This is the first note.",
-        user: @user,
+    let!(:note1) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the first note."
       )
-
-      @note2 = @project.notes.create(
-        message: "This is the second note.",
-        user: @user,
+    }
+    let!(:note2) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the second note."
       )
-
-      @note3 = @project.notes.create(
-        message: "First, preheat the oven",
-        user: @user,
+    }
+    let!(:note3) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "First, preheat the oven."
       )
-    end
+    }
 
     # 一致するデータが見つかる時
     context "when a match is found" do
       # 一致する場合のexampleが並ぶ
       # 検索文字列に一致するメモを返すこと
       it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
+        expect(Note.search("first")).to include(note1, note3)
       end
     end
 
@@ -77,74 +71,9 @@ RSpec.describe Note, type: :model do
       # 一致しない場合のexampleが並ぶ
       # 空のコレクションを返すこと
       it "returns an empty collection" do
-        expect(Note.search("third")).to be_empty
+        expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
-
-# -------------------------------------------------
-
-
-  # # 検索文字列に一致するメモを返すこと
-  # it "returns notes that match the search term" do
-  #   user = User.create(
-  #     first_name: "Joe",
-  #     last_name: "Tester",
-  #     email: "tester@example.com",
-  #     password: "dottle-nouveau-pavilion-tights-furze",
-  #   )
-
-  #   project = user.projects.create(
-  #     name: "Test Project",
-  #   )
-
-  #   note1 = project.notes.create(
-  #     message: "This is the first note.",
-  #     user: user,
-  #   )
-
-  #   note2 = project.notes.create(
-  #     message: "This is the second note.",
-  #     user: user,
-  #   )
-
-  #   note3 = project.notes.create(
-  #     message: "First, preheat the oven",
-  #     user: user,
-  #   )
-
-  #   expect(Note.search("first")).to include(note1, note3)
-  #   expect(Note.search("first")).to_not include(note2)
-  # end
-
-  # # 検索結果が1件も見つからなければ空のコレクションを返すこと
-  # it "returns an empty collection when no results are found" do
-  #   user = User.create(
-  #     first_name: "Joe",
-  #     last_name: "Tester",
-  #     email: "tester@example.com",
-  #     password: "dottle-nouveau-pavilion-tights-furze",
-  #   )
-
-  #   project = user.projects.create(
-  #     name: 'Test Project',
-  #   )
-
-  #   note1 = project.notes.create(
-  #     message: "This is the first note.",
-  #     user: user,
-  #   )
-
-  #   note2 = project.notes.create(
-  #     message: "This is the second note.",
-  #     user: user,
-  #   )
-
-  #   note3 = project.notes.create(
-  #     message: "First, preheat the oven",
-  #     user: user,
-  #   )
-
-  #   expect(Note.search('message')).to be_empty
-  # end
 end
