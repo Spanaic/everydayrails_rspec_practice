83f0f321efe1ae93fd40f3891f01e251f43585a2
Spanaic <spanaic@gmail.com>
2020-06-23 09:18:51 +0900

    shared_contextをprojectスペックに追加

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  before do
-    @user = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, owner: @user)
-    @task = @project.tasks.create!(name: "Test task")
-  end
+  include_context "project setup"
+
+  # let(:user) { FactoryBot.create(:user) }
+  # let(:project) { FactoryBot.create(:project, owner: user) }
+  # let(:task) { project.tasks.create!(name: "Test task") }
+
+  # before do
+  #   @user = FactoryBot.create(:user)
+  #   @project = FactoryBot.create(:project, owner: @user)
+  #   task = @project.tasks.create!(name: "Test task")
+  # end
 
   describe "#show" do
     # JSON形式でレスポンスを返すこと
     it "responds with JSON formatted output" do
-      sign_in @user
+      sign_in user
       get :show, format: :json,
-       params: { project_id: @project.id, id: @task.id }
+       params: { project_id: project.id, id: task.id }
       expect(response.content_type).to eq "application/json"
     end
   end
@@ -21,20 +27,20 @@ RSpec.describe TasksController, type: :controller do
     # JSON形式でレスポンスを返すこと
     it "responds with JSON formatted output" do
       new_task = { name: "New test task" }
-      sign_in @user
+      sign_in user
       post :create, format: :json,
-        params: { project_id: @project.id, task: new_task }
+        params: { project_id: project.id, task: new_task }
       expect(response.content_type).to eq "application/json"
     end
 
     # 新しいタスクをプロジェクトに追加すること
     it "adds a new task to the project" do
       new_task = { name: "New test task" }
-      sign_in @user
+      sign_in user
       expect {
         post :create, format: :json,
-          params: { project_id: @project.id, task: new_task }
-      }.to change(@project.tasks, :count).by(1)
+          params: { project_id: project.id, task: new_task }
+      }.to change(project.tasks, :count).by(1)
     end
 
     # 認証を要求すること
@@ -42,7 +48,7 @@ RSpec.describe TasksController, type: :controller do
       new_task = { name: "New test task" }
       # ここではあえてログインしない...
       post :create, format: :json,
-        params: { project_id: @project.id, task: new_task }
+        params: { project_id: project.id, task: new_task }
       expect(response).to_not be_success
     end
   end
