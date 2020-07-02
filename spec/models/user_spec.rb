855376c434308d9329d0cb0247a1d368fda68640
Spanaic <spanaic@gmail.com>
2020-07-03 08:14:12 +0900

    webサービスのテストコードを追加(VCR対応はせず)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,4 +66,13 @@ RSpec.describe User, type: :model do
     expect(UserMailer).to have_received(:welcome_email).with(user)
   end
 
+  # ジオコーディングを実行すること
+  it "performs geocoding", vcr: true, :record => :new_episodes do
+    user = FactoryBot.create(:user, last_sign_in_ip: "161.185.207.20")
+    expect {
+      user.geocode
+    }.to change(user, :location).
+      from(nil).
+      to("Brooklyn, New York, US")
+  end
 end
